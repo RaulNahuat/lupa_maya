18cbf2f7d700278ad819c690d38b90320b429ea9
RaulNahuat <a22216876@alumnos.uady.mx>
2026-04-26 18:21:15 -0600

    Fix duplicate declaration of glifos in syncService.js

--- src/services/syncService.js
+++ src/services/syncService.js
@@ -34,7 +34,6 @@ export const descargarCambios = async (usuarioLocalId = null) => {
         preguntas = [],
         opciones_respuestas = [],
         nivel_glifos_objetivos = [],
-        glifos = [],
         progreso_usuarios = []
     } = result.cambios;
 
