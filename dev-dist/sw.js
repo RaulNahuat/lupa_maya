6f0f67ea98ba4c04c48b12c496ff1da49b271921
Manuel Cupul <114542854+ManuelC13@users.noreply.github.com>
2026-06-09 15:08:59 -0600

    feat: improve design and functionality of authentication and navigation components

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jral6dq5qog"
+    "revision": "0.l64a94bgnq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
