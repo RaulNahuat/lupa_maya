c3eb649199b890471fb6a0eecd0dfec124f417bf
RaulNahuat <a22216876@alumnos.uady.mx>
2026-06-16 16:15:15 -0600

    feat: implement getGruposNivelesPull controller to fetch all GrupoNivel records

--- server/controllers/pullController/gruposNivelesPullController.js
+++ server/controllers/pullController/gruposNivelesPullController.js
@@ -1,10 +1,5 @@
 export async function getGruposNivelesPull(db, Op, lastSyncDate) {
   const grupos = await db.GrupoNivel.findAll({
-    where: {
-      updated_at: {
-        [Op.gt]: lastSyncDate
-      }
-    },
     raw: true
   });
   return grupos;
