fff67f084cdb9e95cd555ade6b166f0c92dff285
Manuel Cupul <114542854+ManuelC13@users.noreply.github.com>
2026-06-22 09:18:40 -0600

    feat: adjust audio button size

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.nr516r931o8"
+    "revision": "0.l1gqn8dmkv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
